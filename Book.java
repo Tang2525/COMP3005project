0affcfb89ae6fe637e7b7eaf17f840d7e8817e59
Game-DevInProgress <66549060+Game-DevInProgress@users.noreply.github.com>
2022-12-07 01:51:23 -0500

    create Book.java

---
+++ Book.java
@@ -0,0 +1,8 @@
+public class Book {
+    private String title;
+    private int ISBN;
+    private int pages;
+    private float price;
+    private String genre;
+    private String author;
+}
--- main.java
+++ main.java
@@ -1,5 +1,8 @@
+//import the sql here
+
 public class main{
     public static void main(String[] args) {
+        
         System.out.println("Welcome To LookInnaBook");
     }
 }
