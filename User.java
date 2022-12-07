5c9c5a07ade359246b120696e6433092320690f6
Game-DevInProgress <66549060+Game-DevInProgress@users.noreply.github.com>
2022-12-07 02:34:27 -0500

    created constructors for User class

--- User.java
+++ User.java
@@ -3,4 +3,17 @@ public class User{
     String addressInfo;
     String username;
     String password;
+
+
+    public User(){
+        this.name = "guest";
+    }
+
+    public User(String n,String address, String usern, String p){
+        this.name = n;
+        this.addressInfo = address;
+        this.username = usern;
+        this.password = p;
+    }
 }
+
--- main.java
+++ main.java
@@ -1,9 +1,16 @@
 //import the sql here
-
 public class main{
     public static void main(String[] args) {
         User user = new User();
         System.out.println("Welcome To LookInnaBook");
+        System.out.println("logged in as:\t" + user.name);
+    }
+
+    public static void displayMenu(User user){
+        System.out.println("What would you like to do:");
+        System.out.println("1. View books");
+        System.out.println("2. View cart");
+        System.out.println("3. Search for Book");
     }
 }
 
